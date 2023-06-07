2de5f464a839659dfcdc76db3ba309c7895af18c
Shahidul Islam <shahidulislam@Konas-iMac.local>
2023-06-07 23:15:38 +0600

    jwt added

--- index.js
+++ index.js
@@ -2,17 +2,32 @@ const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
 
+const verifyJWT = (req, res, next) => {
+const authorization = req.headers.authorization;
+if(!authorization){
+  return res.status(401).send({error: true, message: 'unautorized access'});
+}
+const token = authorization.split(' ')[1];
+jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+  if(error){
+    return res.status(401).send({error: true, message: 'unautorized access'});
+  }
+  req.decoded = decoded;
+  next()
+})
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ybsvrsr.mongodb.net/?retryWrites=true&w=majority`;
 
 
-// pass
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,7 +41,12 @@ async function run() {
     const primeSportsCollection = client.db('prime-sports').collection('classes&instructor')
     const usersCollection = client.db('prime-sports').collection('users')
 
-   
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '2h' })
+      res.send({ token })
+    })
+
 
     // user collections
     app.get('/users', async (req, res) => {
@@ -36,33 +56,33 @@ async function run() {
 
     app.post('/users', async (req, res) => {
       const user = req.body;
-      const query = {email: user.email}
+      const query = { email: user.email }
       const existingUser = await usersCollection.findOne(query)
-      if(existingUser){
-        return res.send({message: 'user all ready exists'})
+      if (existingUser) {
+        return res.send({ message: 'user all ready exists' })
       }
       const result = await usersCollection.insertOne(user)
       res.send(result)
     })
 
-      app.patch('/users/admin/:id', async (req, res) => {
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const updated = {
         $set: {
-         role : 'admin'
+          role: 'admin'
         }
       };
       const result = await usersCollection.updateOne(query, updated)
       res.send(result)
     })
 
-      app.patch('/users/instructor/:id', async (req, res) => {
+    app.patch('/users/instructor/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const updated = {
         $set: {
-         role : 'instructor'
+          role: 'instructor'
         }
       };
       const result = await usersCollection.updateOne(query, updated)
@@ -84,8 +104,28 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/singleInsructor', verifyJWT, async(req, res)=>{
+      const email = req.query.email;
+      console.log(email)
+      if(!email){
+        res.send([])
+      }
 
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(401).send({error: true, message: 'unautorized access'});
 
+      }
+
+      const query = {email: email};
+      const result = await primeSportsCollection.find(query).toArray();
+      res.send(result)
+    });
+
+    // app.get("/singleInsructor/:email", async (req, res) => {
+    //   const toys = await primeSportsCollection.find({ seller_email: req.params.email, }).toArray();
+    //   res.send(toys);
+    // });
 
     // app.get("/allToys/:id", async (req, res) => {
     //   const id = req.params.id
@@ -94,10 +134,6 @@ async function run() {
     //   res.send(result)
     // })
 
-    // app.get("/singleToys/:email", async (req, res) => {
-    //   const toys = await primeSportsCollection.find({ seller_email: req.params.email, }).toArray();
-    //   res.send(toys);
-    // });
 
 
     // app.get("/searchByTitle/:text", async (req, res) => {
@@ -113,9 +149,8 @@ async function run() {
     //   res.send(result);
     // });
 
-  
-    //   res.send(result);
-    // })
+
+
 
     // app.delete('/allToys/:id', async (req, res) => {
     //   const id = req.params.id;
@@ -124,7 +159,7 @@ async function run() {
     //   res.send(result)
     // })
 
-     // const indexKeys = { toy_name: 1, sub_category: 1 }; 
+    // const indexKeys = { toy_name: 1, sub_category: 1 }; 
     // const indexOptions = { name: "serachByTitle" }; 
     // const result = await carCollection.createIndex(indexKeys, indexOptions);
     // console.log(result);
